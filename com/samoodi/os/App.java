bd314dad92b40effed0fec488bdb40afca05035a
Farhad Uneci <farhaduneci@gmail.com>
2021-01-12 23:05:17 +0330

    Initial Commit

---
+++ com/samoodi/os/App.java
@@ -0,0 +1,17 @@
+/* -------------------------------------------------------------------------- */
+/*                           Reza Samoodi, 970 30 33                          */
+/* -------------------------------------------------------------------------- */
+
+package os;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+
+        // Not necessary needed but not using this will cause resource leak!
+        scanner.close();
+    }
+}
